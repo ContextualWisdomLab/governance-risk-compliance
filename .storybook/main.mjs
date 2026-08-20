8f2ccef7cfa2a8ac39b4ccb2fe50661c84c24de9
Seongho Bae <me@seonghobae.me>
2026-08-20 12:03:46 -0700

    feat(ui): configure Storybook accessibility inventory

---
+++ .storybook/main.mjs
@@ -0,0 +1,9 @@
+/** @type { import('@storybook/web-components-vite').StorybookConfig } */
+const config = {
+  stories: ['../apps/grc-workspace/**/*.stories.mjs'],
+  framework: '@storybook/web-components-vite',
+  addons: ['@storybook/addon-a11y'],
+  docs: { autodocs: true },
+};
+
+export default config;
